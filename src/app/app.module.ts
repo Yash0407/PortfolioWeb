95db9f54338c9e72a81f9106adc126c343063de5
Yash0407 <yasvir.ys.singh@gmail.com>
2024-02-08 01:59:37 -0500

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { CvComponent } from './components/cv/cv.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ReferencesComponent } from './components/references/references.component';
+import { ContactMeComponent } from './components/contact-me/contact-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    CvComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    ReferencesComponent,
+    ContactMeComponent
   ],
   imports: [
     BrowserModule
